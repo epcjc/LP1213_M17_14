525fef75166ca74a7a591f23925e07f292590c56
Tiago Alexandre <i10817@epcjc.net>
2013-05-09 11:49:40 +0100

    Continuação da BD
    Se o setor poder dê uma olhada na BD , tou com alguns problemas com as
    relações.

--- LP_17/LP_17/Menu.cs
+++ LP_17/LP_17/Menu.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace LP_17
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
--- LP_17/LP_17/Program.cs
+++ LP_17/LP_17/Program.cs
@@ -15,7 +15,7 @@ namespace LP_17
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
